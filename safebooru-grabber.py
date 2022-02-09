34d20518a18da179a729aa5a4d1464c05a575707
Turbonator0 <78885232+Turbonator0@users.noreply.github.com>
2022-02-09 11:56:06 +0200

    Reworked command line arguments

--- safebooru-grabber.py
+++ safebooru-grabber.py
@@ -1,17 +1,29 @@
 import wget, requests, sys
 
-def showHelp():
-    print("Syntax is: python3 safebooru-grabber <tag(s)> <output folder>")
-    sys.exit()
+# assumes you are not using a gold/plat account, I have not added auth keys yet
+MAXTAGS = 2
+MAXTAGS_OFFSET = MAXTAGS + 1
 limit = 200
-include_tags = '%20'.join(sys.argv[1:(len(sys.argv)-1)])
-output_folder = sys.argv[len(sys.argv)-1]
-
+include_tags = []
 # This does something
-match sys.argv[1]:
-    case "-h" : showHelp()
-    case "-H" : showHelp()
-    case "help" : showHelp()
+for args in range(len(sys.argv)):
+    current_arg = sys.argv[args]
+    if "--help" in sys.argv:
+        print("""Current options are:
+        -t\t\tfor tags
+        -o\t\tfor output folder
+        --help\t\tto display this message again""")
+        sys.exit()
+    if current_arg == "-o":
+        output_folder = sys.argv[args+1]
+    elif current_arg == "-t":
+        for i in sys.argv[args+1:args+MAXTAGS_OFFSET]:
+            # stop at delimiter which is -
+            if "-" in i:
+                break
+            else: include_tags.append(i)
+    
+include_tags = "%20".join(include_tags)
 
 url = "https://safebooru.donmai.us/posts.json?tags={}&limit={}&page={}"
 while True:
@@ -21,7 +33,7 @@ while True:
         for i in range(200):
             try: 
                 if "file_url" in page_json[i]:
-                    wget.download(page_json[i]["file_url"])
+                    wget.download(page_json[i]["file_url"],output_folder)
             except IndexError:
                 print("Index, not found, quitting...")
                 sys.exit()
